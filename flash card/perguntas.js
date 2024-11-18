6312c94c8827d15010b8e264bc8e9a8f2bfbe5df
samuel junior queiroz lisboa <141160028+samueljuni@users.noreply.github.com>
2024-11-18 08:37:59 -0300

    Add files via upload

---
+++ flash card/perguntas.js
@@ -0,0 +1,39 @@
+criaCartao(
+    '  Literatura',
+    'Quem escreveu "Dom Casmurro?',
+    'Machado de Assis'
+)
+
+criaCartao(
+    'História',
+    ' Quem foi o primeiro imperador romano?',
+    'O primeiro imperador romano foi César Augusto.'
+)
+
+criaCartao(
+    ' Geografia',
+    'Qual é o maior rio do mundo em termos de volume de água',
+    'O maior rio do é o Rio Amazonas'
+)
+
+criaCartao(
+    'Engenharia Civil',
+    ' O que é concreto armado?',
+    '  O concreto armado é um tipo de concreto que é reforçado com barras de aço ou cabos de aço para aumentar sua resistência à tração.'
+)
+
+
+criaCartao(
+    'Geografia / Biologia',
+    'Qual é o nome da maior floresta tropical do mundo?',
+    'Floresta Amazônica'
+)
+
+
+criaCartao(
+    'matematica',
+    'O que é um número primo?',
+    ' são números primos que incluem 2, 3, 5, 7, 11, entre outros.'
+)
+
+
